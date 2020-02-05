8c58f78c6c825d90932e89ed3e4e58a227efbe4c
taran <taran.goel@nagarro.com>
2020-02-05 12:28:16 -0500

    `

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
     def home
-        @greeting = "Home action says: Hello world!"
+        @greeting = "Home action says: Hello world! The Pipeline is now working :)"
     end
 end
